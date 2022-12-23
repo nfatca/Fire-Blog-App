0351d8a3f7dade3ef020ac4d154daea2437670a3
Nurullah Furkan Atca <nfurkanatca@gmail.com>
2022-12-23 19:23:11 +0300

    state

--- src/contexts/AuthContectProvider.jsx
+++ src/contexts/AuthContectProvider.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, createContext } from "react";
+import { auth, googleProvider } from "../helpers/firebase";
 
 //! Create context for authentication data
 const AuthContect = createContext();
@@ -9,7 +10,39 @@ export function useAuth() {
 }
 
 const AuthContectProvider = () => {
-  return <div>AuthContectProvider</div>;
+  function signup(email, password) {
+    return auth.createUserWithEmailAndPassword(email, password);
+  }
+
+  function login(email, password) {
+    return auth.signInWithEmailAndPassword(email, password);
+  }
+
+  function logout() {
+    auth.signOut(auth);
+  }
+
+  function loginWithGoogle() {
+    googleProvider.setCustomParameters({ prompt: "select_account" });
+    auth.signInWithPopup(googleProvider);
+  }
+
+  function resetPassword(email) {
+    return auth.sendPasswordResetEmail(email);
+  }
+  function updateEmail(email) {
+    return currentUser.updateEmail(email);
+  }
+
+  const values = {
+    signup,
+    login,
+    logout,
+    loginWithGoogle,
+    resetPassword,
+    updateEmail,
+  };
+  return <AuthContect.Provider value={values}>{children}</AuthContect.Provider>;
 };
 
 export default AuthContectProvider;
